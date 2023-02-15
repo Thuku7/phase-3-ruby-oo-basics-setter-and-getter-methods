64430e5b91f9d97122acbe077f178d941b539505
Robert <robethuku11@gmail.com>
2023-02-15 16:14:54 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,35 @@
 class Dog
+  #dog name setter
+  def name=(name)
+    @name = name
+  end
+
+  #dog breed setter
+  def breed=(breed)
+    @breed = breed
+  end
+
+  #dog name getter
+  def name 
+    @name
+  end
+
+  #dog breed getter
+  def breed
+    @breed
+  end
+
+  
 
 end
+
+fido = Dog.new
+snoopy = Dog.new
+
+fido.name  = "Fido"
+snoopy.breed = "Beagle"
+
+puts fido.name
+puts snoopy.breed
+
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,33 @@
 class Person
+  #name setter menthod
+  def name=(name)
+    @name = name
+  end
+  #job setter method
+  def job=(job)
+    @job = job
+  end
+
+  #name getter method
+  def name
+    @name
+  end
+
+  #job getter method
+  def job
+    @job
+  end
 
 end
+
+
+beyonce = Person.new
+
+beyonce.name = "Beyonce"
+beyonce.job = "Singer"
+
+puts beyonce.name
+puts beyonce.job
+
+
+
